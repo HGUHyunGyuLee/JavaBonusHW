429c0b70ead4d6b863858de97734a1e62d42e12a
lhyun <lhyun@192.168.0.7>
2019-06-19 07:02:10 +0900

    last commit

--- src/main/java/edu/handong/csee/java/ls.java
+++ src/main/java/edu/handong/csee/java/ls.java
@@ -37,7 +37,7 @@ public class ls {
 			if (a) {
 				int i = 0;
 				ArrayList<String> subFilesNames = new ArrayList<String>();
-				for (String fileName : f.list()) 
+				for (String fileName : f.list())
 					subFilesNames.add(fileName);
 				Collections.sort(subFilesNames);
 				for (String fileNames : subFilesNames) {
@@ -49,17 +49,19 @@ public class ls {
 				return;
 			}
 			if (m) {
-				int i=0;
+				int i = 0;
 				String temp[] = f.list();
 				for (String fileName : f.list()) {
 					i++;
-					if(i!=temp.length)System.out.print(fileName + ", ");
-					else System.out.print(fileName);
+					if (i != temp.length)
+						System.out.print(fileName + ", ");
+					else
+						System.out.print(fileName);
 				}
 			}
 			if (d) {
 				for (String fileName : f.list()) {
-					if(fileName.equals(dArg)) {
+					if (fileName.equals(dArg)) {
 						System.out.print(fileName);
 						return;
 					}
@@ -68,15 +70,16 @@ public class ls {
 			}
 			if (r) {
 				ArrayList<String> subFilesNames = new ArrayList<String>();
-				
+
 				for (String fileName : f.list()) {
 					subFilesNames.add(fileName);
 				}
-				
+
 				Collections.sort(subFilesNames, Collections.reverseOrder());
-				int i=0;
+				int i = 0;
 				for (String fileNames : subFilesNames) {
-					if(!fileNames.startsWith("."))System.out.print(fileNames + "\t");
+					if (!fileNames.startsWith("."))
+						System.out.print(fileNames + "\t");
 					i++;
 					if (i % 5 == 0)
 						System.out.println();
@@ -84,36 +87,35 @@ public class ls {
 			}
 
 			if (S) {
-				int i=0;
+				int i = 0;
 				ArrayList<Integer> subFilesSize = new ArrayList<Integer>();
 				HashMap<Integer, String> subFileHash = new HashMap<Integer, String>();
 				for (String fileName : f.list()) {
 					String tempFilePath = path + "/" + fileName;
 					File tempFile = new File(tempFilePath);
-					if(!subFilesSize.contains((int) tempFile.length()))subFilesSize.add((int) tempFile.length());
-					if(subFileHash.containsKey((int) tempFile.length())) 
-					{
-						if(subFileHash.get((int) tempFile.length()).compareTo(fileName)==1) {
-							subFileHash.put((int) tempFile.length()-1, fileName);
-							subFilesSize.add((int) tempFile.length()-1);
+					if (!subFilesSize.contains((int) tempFile.length()))
+						subFilesSize.add((int) tempFile.length());
+					if (subFileHash.containsKey((int) tempFile.length())) {
+						if (subFileHash.get((int) tempFile.length()).compareTo(fileName) == 1) {
+							subFileHash.put((int) tempFile.length() - 1, fileName);
+							subFilesSize.add((int) tempFile.length() - 1);
+						} else if ((subFileHash.get((int) tempFile.length()).compareTo(fileName) == -1)) {
+							subFileHash.put((int) tempFile.length() + 1, fileName);
+							subFilesSize.add((int) tempFile.length() + 1);
 						}
-						else if((subFileHash.get((int) tempFile.length()).compareTo(fileName)==-1)) {
-							subFileHash.put((int) tempFile.length()+1, fileName);
-							subFilesSize.add((int) tempFile.length()+1);
-						}
-					}
-					else subFileHash.put((int) tempFile.length(), fileName);
-					
+					} else
+						subFileHash.put((int) tempFile.length(), fileName);
+
 				}
 				Collections.sort(subFilesSize, Collections.reverseOrder());
 				for (Integer fileSize : subFilesSize) {
-				//	i++;
-					if(!subFileHash.get(fileSize).startsWith(".")) {
+					// i++;
+					if (!subFileHash.get(fileSize).startsWith(".")) {
 						System.out.print(subFileHash.get(fileSize) + "\t");
 						i++;
 					}
 					if (i % 5 == 0)
-						System.out.println(); 
+						System.out.println();
 				}
 
 			}
@@ -128,7 +130,6 @@ public class ls {
 
 			CommandLine cmd = parser.parse(options, args);
 
-			// path = cmd.getOptionValue("p");
 			a = cmd.hasOption("a");
 			m = cmd.hasOption("m");
 			S = cmd.hasOption("S");
@@ -174,8 +175,8 @@ public class ls {
 				// .required() // this is an optional option. So disabled required().
 				.build());
 		options.addOption(Option.builder("d").desc("Display the file if exists").hasArg() // this option is intended not
-																							// to have an option value
-																							// but just an
+				// to have an option value
+				// but just an
 				// option
 				.argName("Diplay a specific file")
 				// .required() // this is an optional option. So disabled required().
